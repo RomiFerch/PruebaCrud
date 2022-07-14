3e74f6d8127695e9aa171fdaa2073feb3992529d
RomiFerCh <romi.fer@icloud.com>
2022-07-14 17:41:10 -0400

    lo que hay

--- code.js
+++ code.js
@@ -18,10 +18,23 @@ const CrearItem = (actividades) => {
 const GuardarDB = () => {
     localStorage.setItem('tareas', JSON.stringify(arrayActividades))
 
+    LeerDB()
+
 }
 const LeerDB = () => {
     listaActividadesUI.innerHTML = '';
 
+    arrayActividades = JSON.parse(localStorage.getItem('tareas'))
+    
+    if (arrayActividades === null) {
+        arrayActividades = []
+    }else {
+
+        arrayActividades.forEach(element => {
+            listaActividadesUI.innerHTML +=`<div class="alert alert-primary" role="alert"><b>${element.actividad}</b> - ${element.estado}
+<span class="mx-3 float-end"><i class="fa-solid fa-pen-to-square mx-2"> edit </i><i class="fa-solid fa-trash-can mx-2"> eliminar </i></span></div>`            
+        });
+    }
 }
 
 formularioUI.addEventListener('submit', (e)=>{
@@ -37,3 +50,15 @@ formularioUI.addEventListener('submit', (e)=>{
   
 })
 document.addEventListener('DOMContentLoaded', LeerDB)
+
+listaActividadesUI.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    console.log(e.path[2].childNodes[0].innerHTML);
+
+    console.log(e.target.innerHTML);
+
+    if(e.target.innerHTML === 'edit' ||e.target.innerHTML === 'eliminar' ){
+        console.log('accion de edit');
+    }
+});
